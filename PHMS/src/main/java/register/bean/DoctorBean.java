65c2dd5668a6764921fc84b7f6c55677e257a8ec
aravindhbhaskar <aravindhbhaskar@aravindhs-air>
2023-04-05 22:55:16 +0530

    First Commit

---
+++ PHMS/src/main/java/register/bean/DoctorBean.java
@@ -0,0 +1,89 @@
+package register.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "TABLE")
+public class DoctorBean {
+	@Autowired
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private String FirstName;
+	private String LastName;
+	private int Age;
+	private String Gender;
+	private int ContactNumber;
+	private String EmailAddress;
+	private String city;
+	private String state;
+	private String UserId;
+	private String Password;
+
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public String getGender() {
+		return Gender;
+	}
+	public void setGender(String gender) {
+		Gender = gender;
+	}
+	public long getContactNumber() {
+		return ContactNumber;
+	}
+	public void setContactNumber(int contactNumber) {
+		ContactNumber = contactNumber;
+	}
+	public String getEmailAddress() {
+		return EmailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		EmailAddress = emailAddress;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getUserId() {
+		return UserId;
+	}
+	public void setUserId(String userId) {
+		UserId = userId;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	
+}
